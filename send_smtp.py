d61cce827db13aba152e4e77ef3ccda8eee9fdca
zianazari <40858610+zianazari@users.noreply.github.com>
2022-09-10 21:46:59 +0430

    first commit
    sending emails using SMTP on SSL

---
+++ send_smtp.py
@@ -0,0 +1,41 @@
+import socket
+
+HOST, PORT = "mail.domain1.com", 587
+sender_email = "user1@domain1.com"
+receiver_email = "user2@domain1.com"
+forged_sender_email = "user1@domain2.com"
+forged_receiver_email = receiver_email
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+client_socket.settimeout(5)
+print("--------------------------------------------------------------")
+try:
+    print("Server:  " + client_socket.recv(1024).decode("utf-8"), end="")
+
+    client_socket.send("HELO        example.com\n".encode("utf-8"))
+    print("Client:      HELO example.com\n")
+    print("Server:      " + client_socket.recv(1024).decode("utf-8"), end="")
+
+    client_socket.send(("MAIL FROM:      " + sender_email + "\n").encode("utf-8"))
+    print("Client:      MAIL FROM: " + sender_email + "\n")
+    print("Server:      " + client_socket.recv(1024).decode("utf-8"), end="")
+
+    client_socket.send(("RCPT TO:      " + receiver_email + "\n").encode("utf-8"))
+    print("Client:      RCPT TO: " + receiver_email + "\n")
+    print("Server:      " + client_socket.recv(1024).decode("utf-8"), end="")
+
+    msg = "Plz click on the following link\n"
+    data = "DATA\n\
+           Subject: new project\n\
+           From: " + forged_sender_email + "\n\
+           To: " + forged_receiver_email + "\n\
+           " + msg + "\n\
+           .\n"
+    client_socket.send(msg.encode("utf-8"))
+    print("Server:      " + client_socket.recv(1024).decode("utf-8"), end="")
+
+except TimeoutError:
+    print("timeout")
+
+client_socket.close()
+print("socket is down!")
---
+++ send_smtp_ssl.py
@@ -0,0 +1,47 @@
+import socket
+import ssl
+
+HOST, PORT = "mail.domain1.com", 465
+sender_email = "user1@domain1.com"
+receiver_email = "user2@domain1.com"
+forged_sender_email = "user1@domain2.com"
+forged_receiver_email = receiver_email
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.settimeout(5)
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+ssl_context.verify_mode = ssl.CERT_NONE
+wrapped_socket = ssl_context.wrap_socket(client_socket, server_hostname=HOST)
+wrapped_socket.connect((HOST, PORT))
+
+print("--------------------------------------------------------------")
+try:
+    print("Server:  " + wrapped_socket.recv(1024).decode("utf-8"), end="")
+
+    wrapped_socket.send("HELO        example.com\n".encode("utf-8"))
+    print("Client:      HELO example.com\n")
+    print("Server:      " + wrapped_socket.recv(1024).decode("utf-8"), end="")
+
+    wrapped_socket.send(("MAIL FROM:      " + sender_email + "\n").encode("utf-8"))
+    print("Client:      MAIL FROM: " + sender_email + "\n")
+    print("Server:      " + wrapped_socket.recv(1024).decode("utf-8"), end="")
+
+    wrapped_socket.send(("RCPT TO:      " + receiver_email + "\n").encode("utf-8"))
+    print("Client:      RCPT TO: " + receiver_email + "\n")
+    print("Server:      " + wrapped_socket.recv(1024).decode("utf-8"), end="")
+
+    msg = "Plz click on the following link\n"
+    data = "DATA\n\
+           Subject: new project\n\
+           From: " + forged_sender_email + "\n\
+           To: " + forged_receiver_email + "\n\
+           " + msg + "\n\
+           .\n"
+    wrapped_socket.send(msg.encode("utf-8"))
+    print("Server:      " + wrapped_socket.recv(1024).decode("utf-8"), end="")
+
+except TimeoutError:
+    print("timeout")
+
+wrapped_socket.close()
+print("socket is down!")
